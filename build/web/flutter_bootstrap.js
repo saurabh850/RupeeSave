42a81ab7c2c765359ef83b86794afe769f4ccb08
Saurabh Thakur <shaurabhnarayan@gmail.com>
2025-12-05 02:55:16 +0530

    Chore: Add cache buster to index.html

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "852533701"
+    serviceWorkerVersion: "3127560521"
   }
 });
